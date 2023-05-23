83823708027efc9661a057e80049744829876b5a
Newdaymonthyear <93859467+Newdaymonthyear@users.noreply.github.com>
2023-05-23 12:37:40 +0800

    Update GitHubShareButton.tsx

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+/*import * as React from 'react'
 
 import styles from './styles.module.css'
 
@@ -45,4 +45,4 @@ export const GitHubShareButton: React.FC = () => {
       </svg>
     </a>
   )
-}
+}*/
